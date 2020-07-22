16e62668679b238d6bafff37d71586dc189192d3
Gareth Coles <gdude2002@gmail.com>
2020-07-22 23:40:42 +0100

    Spawning rules

--- src/main/java/rocks/sakira/flamingo/Flamingo.java
+++ src/main/java/rocks/sakira/flamingo/Flamingo.java
@@ -2,7 +2,9 @@ package rocks.sakira.flamingo;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.SpriteRenderer;
+import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntitySpawnPlacementRegistry;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -14,6 +16,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 import rocks.sakira.flamingo.client.render.RenderFlamingo;
 import rocks.sakira.flamingo.entity.EntityFlamingo;
 import rocks.sakira.flamingo.register.Entities;
@@ -35,13 +38,26 @@ public class Flamingo {
     }
 
     @SubscribeEvent
-    public void setupCommon(final FMLCommonSetupEvent event) throws IllegalAccessException, NoSuchFieldException {
+    public void setupCommon(final FMLCommonSetupEvent event) {
         EntitySpawnPlacementRegistry.register(
                 Entities.FLAMINGO_ENTITY.get(),
                 EntitySpawnPlacementRegistry.PlacementType.ON_GROUND,
                 Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
                 EntityFlamingo::canAnimalSpawn
         );
+
+        for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
+            if (biome.getCategory() == Biome.Category.SWAMP) {
+                biome.getSpawns(EntityClassification.CREATURE).add(
+                        new Biome.SpawnListEntry(
+                                Entities.FLAMINGO_ENTITY.get(),
+                                15,
+                                5,
+                                12
+                        )
+                );
+            }
+        }
     }
 
     @SubscribeEvent
